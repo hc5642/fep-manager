33919c17f596873f12260ca1831ea56354c30721
hc5642 <hc5642@me.com>
2023-04-07 16:11:51 +0900

    2ND COMMIT

---
+++ fep-manager/src/test/java/com/kbk/fep/FepManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbk.fep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FepManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
