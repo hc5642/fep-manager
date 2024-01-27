11f81ae6507ab7f48017cd58706b304a314bc62b
2310449 <hyonchul.oh@bok.or.kr>
2024-01-27 11:49:30 +0900

    https sample project init()

---
+++ bokworks-activemq-sample/src/main/java/com/bok/ohc/amq/BokworksActivemqSampleApplication.java
@@ -0,0 +1,13 @@
+package com.bok.ohc.amq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokworksActivemqSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokworksActivemqSampleApplication.class, args);
+	}
+
+}
---
+++ bokworks-activemq-sample/src/test/java/com/bok/ohc/amq/BokworksActivemqSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bok.ohc.amq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokworksActivemqSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
